b042b97eacd379972fabe0b07a88abec62f6509f
sadeghranjbar <123ranjbar123@gmail.com>
2024-07-30 18:15:08 +0330

    upload 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
 import "./App.css";
 const tg = window.Telegram.WebApp;
 
 function App() {
+
+  useEffect(() => {
+
+    tg.ready();
+
+  }, [])
+
+
   const onClose = () => {
-    console.log(EventTargetShim);
-  };
+    tg.close()
+  }
 
   return (
     <div className="App">
