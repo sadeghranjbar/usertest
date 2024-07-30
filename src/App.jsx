f2307b582e602715b2d875a0ddf1bafc85ed2ad3
sadeghranjbar <123ranjbar123@gmail.com>
2024-07-30 18:28:35 +0330

    upload 4

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      work latest
+      work latest username injast
       <button onClick={onClose}> close </button>
       <span>{tg.initDataUnsafe?.user?.username}</span>
     </div>
